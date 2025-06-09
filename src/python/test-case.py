12a2c52870edd839352297e0091ad5ae6fcc4c11
Soren Archibald <soren@qbizinc.com>
2025-06-09 12:35:48 -0700

    Adding code that should be searched.

---
+++ src/python/test-case.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+http_bearer_authentication_header  = "EQdaIpx6J6Td6wWIHAxgfxuWg4S3DSpBRCq0Ltz4"
+
+print(f"Here is the value for  my `http_bearer_authentication_header`:  {http_bearer_authentication_header}")
